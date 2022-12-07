152f1a8e4ca225954468eba1aad6bddd49cc94cd
Dattle31 <ddattoli@hotmail.com>
2022-12-08 10:20:28 +1100

    Updates

---
+++ precache-manifest.1cfc528591b2c910e49c879d09178a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64711c5c62ebd12fa8a9",
+    "url": "/tasmanian-trouts-react/static/css/main.89fd77e3.chunk.css"
+  },
+  {
+    "revision": "64711c5c62ebd12fa8a9",
+    "url": "/tasmanian-trouts-react/static/js/main.64711c5c.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/tasmanian-trouts-react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/tasmanian-trouts-react/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "56e754a8d969b07257aa",
+    "url": "/tasmanian-trouts-react/static/js/runtime~main.56e754a8.js"
+  },
+  {
+    "revision": "3478a4bd8776a393e1feeb72fa19d1c2",
+    "url": "/tasmanian-trouts-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.io/tasmanian-trouts---react/precache-manifest.f92f47ec73509b77fb32b23f3a7d0141.js"
+  "/tasmanian-trouts-react/precache-manifest.1cfc528591b2c910e49c879d09178a2c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.io/tasmanian-trouts---react/index.html", {
+workbox.routing.registerNavigationRoute("/tasmanian-trouts-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
