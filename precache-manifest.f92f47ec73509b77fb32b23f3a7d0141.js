2d7f92b6c03388c17a76590abaaf5136a6149909
Dattle31 <ddattoli@hotmail.com>
2022-12-07 20:27:55 +1100

    Updates

---
+++ precache-manifest.f92f47ec73509b77fb32b23f3a7d0141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64711c5c62ebd12fa8a9",
+    "url": "/github.io/tasmanian-trouts---react/static/css/main.89fd77e3.chunk.css"
+  },
+  {
+    "revision": "64711c5c62ebd12fa8a9",
+    "url": "/github.io/tasmanian-trouts---react/static/js/main.64711c5c.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/github.io/tasmanian-trouts---react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/github.io/tasmanian-trouts---react/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "07840361eef36597185e",
+    "url": "/github.io/tasmanian-trouts---react/static/js/runtime~main.07840361.js"
+  },
+  {
+    "revision": "88cec37a5b039026121851bb67e20bcb",
+    "url": "/github.io/tasmanian-trouts---react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/github.io/tasmanian-trouts---react/precache-manifest.f92f47ec73509b77fb32b23f3a7d0141.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/github.io/tasmanian-trouts---react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
